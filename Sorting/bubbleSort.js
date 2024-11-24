f9341402433d5821f382385cbe29b49f40d989f3
Mahendra Chinthamgari <mahendrachinthamgari@gmail.com>
2024-11-25 00:25:07 +0530

    Corrected sort.

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,14 +1,15 @@
 function bubbleSort(arr) {
     for(let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++ ) {
-            if (arr[i] > arr[j]) {
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+        for (let j = 0; j < arr.length - i - 1; j++ ) {
+            console.log(arr)
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
     console.log(arr)
 }
 
-bubbleSort([9, 8, -13, 5]);
+bubbleSort([10, 8, 20, 5]);
